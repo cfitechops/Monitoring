090322a28f49f80ec41e5983ac2eafa5c9ea39fd
cfitechops <tbarry@cfitech.be>
2025-02-19 10:40:54 +0100

    Prometheus Grafana Linux

--- Prometheus-Grafana-Linux/node_exporter/node_exporter_setup_commands.sh
+++ Prometheus-Grafana-Linux/node_exporter/node_exporter_setup_commands.sh
@@ -1,13 +1,28 @@
-wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
-tar xvfz node_exporter-*.*-amd64.tar.gz
-cp node_exporter-*.*-amd64/node_exporter /usr/local/bin
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.0/node_exporter-1.9.0.linux-amd64.tar.gz
+#tar xvfz node_exporter-*.*-amd64.tar.gz
 
-cp node_exporter.service /etc/systemd/system/node_exporter.service
+sudo cp node_exporter-*.*-amd64/node_exporter /usr/local/bin
 
 cd /usr/local/bin
 
-useradd -r node_exporter
+nano /etc/systemd/system/node_exporter.service
 
+```sh
+[Unit]
+Description=Prometheus Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter  
+Group=node_exporter
+ExecStart=/usr/local/bin/node_exporter
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+```
+useradd -r node_exporter
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
 sudo systemctl enable node_exporter
+
